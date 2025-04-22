526b7d6f8ea739538324f24cc60537c20607a98b
Amel Bakouri <amel.bakouri@etu.univ-amu.fr>
2025-04-22 12:27:34 +0200

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nobis
+        fugit aliquid deleniti a eos ipsum, sed incidunt distinctio nisi enim ab
+        error voluptatem, odio quis ipsa, blanditiis magni! Consequatur atque
+        culpa ea laboriosam officia recusandae doloribus voluptas quos dolorum
+        enim. Optio suscipit, aut sint voluptates laudantium quasi rerum fugiat.
+      </span>
+    </div>
+  );
+};
+
+export default HomePage;
